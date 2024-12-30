d6ad626eefe9155cf9a5c338908b3ff0ca591057
tomnemec <codin.with.tom@gmail.com>
2024-12-30 20:54:47 +0100

    refactor ShoppingDialog component styles and button interaction for improved user experience

--- src/components/ShoppingDialog/ShopppingDialog.tsx
+++ src/components/ShoppingDialog/ShopppingDialog.tsx
@@ -1,14 +1,17 @@
-import { CiCreditCard1 } from "react-icons/ci"
+import { CiCreditCard1 } from "react-icons/ci";
 
 const ShopppingDialog = () => {
   return (
     <div className="bg-white shadow-md flex flex-col gap-2 p-5 w-72 h-56 rounded-3xl ">
-        <span className="font-semibold text-2xl">Vitamins AllRounder</span>
-        <span className="text-slate-400">250mg</span>
-        <span className="font-semibold text-lg">$25.00</span>
-        <button><CiCreditCard1 />Buy Now</button>
+      <span className="font-semibold text-2xl">Vitamins AllRounder</span>
+      <span className="text-slate-400">250mg</span>
+      <span className="font-semibold text-2xl">$25.00</span>
+      <button className="flex gap-1 items-center justify-center bg-yellow-400 rounded-full p-2 font-semibold mt-4 hover:scale-105 transition-all cursor-pointer">
+        <CiCreditCard1 className="text-2xl" />
+        Buy Now
+      </button>
     </div>
-  )
-}
+  );
+};
 
-export default ShopppingDialog
+export default ShopppingDialog;
